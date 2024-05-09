06b53b791846e81a24827cb26b96369f9e090275
Sarah Rodrigues da Cruz <44931066895@>
2024-05-09 16:43:41 -0300

    editando e excluindo usuário

--- Cadastro/App_Cadastro/urls.py
+++ Cadastro/App_Cadastro/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.abre_index, name='abre_index'),
-    path('cadastrarPessoa', views.cadastrarPessoa, name='cadastrarPessoa'),
+    # path('', views.abre_index, name='abre_index'),
+    path('', views.cadastrarPessoa, name='cadastrarPessoa'),
+    path('editarinfo/<int:id>', views.editarInfo, name='editarInfo'),
+    path('excluirPessoa/<int:id>', views.excluirPessoa, name='excluir')
 ]
--- Cadastro/App_Cadastro/views.py
+++ Cadastro/App_Cadastro/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from .models import *
 
 # Create your views here.
-def abre_index(request):
-    return render(request, 'index.html')
+# def abre_index(request):
+#     return render(request, 'index.html')
 
 def cadastrarPessoa(request):
+    mensagem = ''
+    pessoas = Pessoa.objects.all()
     if (request.method == 'POST'):
         nomeCadastro = request.POST.get('nome')
         idadeCadastro = request.POST.get('idade')
@@ -23,6 +27,27 @@ def cadastrarPessoa(request):
         pessoas = Pessoa.objects.all()
         print(f"Tudo de pessoa: {pessoas}")
 
-        return render(request, 'index.html', {'mensagem': mensagem, 'pessoas': pessoas})
+    return render(request, 'index.html', {'mensagem': mensagem, 'pessoas': pessoas})
     
+def excluirPessoa(request, id):
+    print("cheguei aqui")
+    pessoa = Pessoa.objects.get(pk=id)
+    pessoa.delete()
+   
+    return HttpResponseRedirect(reverse("cadastrarPessoa"))
+ 
+def editarInfo(request,id):
+    pessoa = Pessoa.objects.get(pk=id)
+    if(request.method == 'POST'):
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+       
+        pessoa.nome = nome
+        pessoa.idade = idade
+       
+        pessoa.save()
+       
+        return HttpResponseRedirect(reverse("cadastrarPessoa"))
+   
+    return render(request, 'edicao.html', {'pessoa':pessoa})
     
